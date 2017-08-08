7b0a91f1d09eaede1fb99810615b8fb083207580
Tim Allison <git.pushes@timawesomeness.com>
2017-08-07 23:10:14 -0600

    Added median scores

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import praw, config, re, time, prawcore, string
 from datetime import datetime
+from statistics import median
 
 def main():
     reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret, user_agent=config.user_agent, username=config.username, password=config.password) 
@@ -64,13 +65,11 @@ def reply_stats(comment, username, reddit):
 def compile_stats(posts, comments, username):
     # Post vars:
     oldest_post = None
-    count_posts = 0
-    score_posts = 0
+    score_posts = []
     words_posts = 0
     # Comment vars:
     oldest_comment = None
-    count_comments = 0
-    score_comments = 0
+    score_comments = []
     words_comments = 0
 
     for post in posts:
@@ -79,8 +78,7 @@ def compile_stats(posts, comments, username):
                 oldest_post = post.created_utc
         else:
             oldest_post = post.created_utc
-        count_posts += 1
-        score_posts += post.score
+        score_posts.append(post.score)
         words_posts += len(re.sub('[' + string.punctuation + ']', '', post.title).split())
 
     for comment in comments:
@@ -89,11 +87,15 @@ def compile_stats(posts, comments, username):
                 oldest_comment = comment.created_utc
         else:
             oldest_comment = comment.created_utc
-        count_comments += 1
-        score_comments += comment.score
+        score_comments.append(comment.score)
         words_comments += len(re.sub('[' + string.punctuation + ']', '', comment.body).split())
     
-    if count_comments == 0 and count_posts == 0:
+    count_posts = len(score_posts)
+    count_comments = len(score_comments)
+    total_score_posts = sum(score_posts)
+    total_score_comments = sum(score_comments)
+
+    if count_posts == 0 and count_comments == 0:
         return "Error. User appears to have 0 posts and 0 comments."
 
     current_time = datetime.now().timestamp()
@@ -104,22 +106,26 @@ def compile_stats(posts, comments, username):
         oldest_comment = current_time
 
     stats_string = (
-        f"**Stats for {username}:**\n\n"                                                                                                                            # For readability:
-        f"**Over the last {str(count_posts)} posts (over {str(round((current_time - oldest_post) / 86400))} days):**  \n"                                           # Post header
-        f"Total post upvotes: {str(score_posts - count_posts)}  \n"                                                                                                 #  -Post score
-        f"Average post score: {str(round(score_posts / count_posts) if count_posts > 0 else 0)}  \n"                                                                #  -Avg. post score
-        f"Average post upvotes/day: {str(round((score_posts - count_posts) / ((current_time - oldest_post) / 86400)) if current_time != oldest_post else 0)}  \n"   #  -Post score/day
-        f"Average posts/day: {str(round(count_posts / ((current_time - oldest_post) / 86400), 1) if current_time != oldest_post else 0)}  \n"                       #  -Posts/day
-        f"Total words in post titles: {str(words_posts)}  \n"                                                                                                       #  -Title words
-        f"Average words per post title: {str(round(words_posts / count_posts) if count_posts > 0 else 0)}\n\n"                                                      #  -Avg. title words
-        f"**Over the last {str(count_comments)} comments (over {str(round((current_time - oldest_comment) / 86400))} days):**  \n"                                  # Comment header
-        f"Total comment upvotes: {str(score_comments - count_comments)}  \n"                                                                                        #  -Comment score
-        f"Average comment score: {str(round(score_comments / count_comments) if count_comments > 0 else 0)}  \n"                                                    #  -Avg. comment score
-         "Average comment upvotes/day: "                                                                                                                            #  -Comment score/day
-            f"{str(round((score_comments - count_comments) / ((current_time - oldest_comment) / 86400)) if current_time != oldest_comment else 0)}  \n"
-        f"Average comments/day: {str(round(count_comments / ((current_time - oldest_comment) / 86400), 1) if current_time != oldest_comment else 0)}  \n"           #  -Comments/day
-        f"Total words in comments: {str(words_comments)}  \n"                                                                                                       #  -Comment words
-        f"Average words per comment: {str(round(words_comments / count_comments) if count_comments > 0 else 0)}"                                                    #  -Avg. comment words
+        f"**Stats for {username}:**\n\n"                                                                                                                        # For readability:
+        f"**Over the last {str(count_posts)} posts (over {str(round((current_time - oldest_post) / 86400))} days):**  \n"                                       # Post header
+        f"Total post upvotes: {str(total_score_posts - count_posts)}  \n"                                                                                       #  -Post score
+        f"Average post score: {str(round(total_score_posts / count_posts) if count_posts > 0 else 0)}  \n"                                                      #  -Avg. post score
+        f"Median post score: {str(round(median(score_posts)) if count_posts > 0 else 0)}  \n"                                                                   #  -Med. post score
+         "Average post upvotes/day: "                                                                                                                           #  -Post score/day
+            f"{str(round((total_score_posts - count_posts) / ((current_time - oldest_post) / 86400)) if current_time != oldest_post else 0)}  \n"   
+        f"Average posts/day: {str(round(count_posts / ((current_time - oldest_post) / 86400), 1) if current_time != oldest_post else 0)}  \n"                   #  -Posts/day
+        f"Total words in post titles: {str(words_posts)}  \n"                                                                                                   #  -Title words
+        f"Average words per post title: {str(round(words_posts / count_posts) if count_posts > 0 else 0)}\n\n"                                                  #  -Avg. title words
+                                                                                                                                                                #----------------------
+        f"**Over the last {str(count_comments)} comments (over {str(round((current_time - oldest_comment) / 86400))} days):**  \n"                              # Comment header
+        f"Total comment upvotes: {str(total_score_comments - count_comments)}  \n"                                                                              #  -Comment score
+        f"Average comment score: {str(round(total_score_comments / count_comments) if count_comments > 0 else 0)}  \n"                                          #  -Avg. comment score
+        f"Median comment score: {str(round(median(score_comments)) if count_comments > 0 else 0)}  \n"                                                          #  -Med. comment score
+         "Average comment upvotes/day: "                                                                                                                        #  -Comment score/day
+            f"{str(round((total_score_comments - count_comments) / ((current_time - oldest_comment) / 86400)) if current_time != oldest_comment else 0)}  \n"
+        f"Average comments/day: {str(round(count_comments / ((current_time - oldest_comment) / 86400), 1) if current_time != oldest_comment else 0)}  \n"       #  -Comments/day
+        f"Total words in comments: {str(words_comments)}  \n"                                                                                                   #  -Comment words
+        f"Average words per comment: {str(round(words_comments / count_comments) if count_comments > 0 else 0)}"                                                #  -Avg. comment words
     ) 
     return stats_string
 
